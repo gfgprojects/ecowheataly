686e3108eaf5f34325bcbd6793f6f0e1f8fffdfa
Arianna Di Paola <arianna.dipaola@ibe.cnr.it>
2025-03-10 16:11:37 +0100

    aggiunta piccola variazione per assegnare ai flussi di biosphere i valori (if any) dei fitofarmaci usati nella RICA.

--- task1_2/scripts/4_lca.py
+++ task1_2/scripts/4_lca.py
@@ -5,21 +5,42 @@ import bw2data as bd
 import bw2calc as bc
 import numpy as np
 import pandas as pd
+from  task1_2.scripts.utils import get_pesticide_info
+
 
 # Step 1: Provide inputs for 1 hectare
 hours_of_tractor_use = 2.5
-kg_of_nitrogen = 50
-Fungicide_harmful = 0
-Herbicide_caution = 0
-Herbicide_irritating = 0
-Herbicide_harmful = 0
-GrowthRegulator_harmful = 0
-Insecticide_harmful = 0
-Insecticide_toxic = 0.13
-Molluscicide_irritating = 0
 MJ = 100 / 0.2779 * hours_of_tractor_use  # Tractor energy calculation
 
-# Step 2: Create the EcoWheataly production activity
+kg_of_nitrogen = 50
+
+# Step 1: Definizione delle variabili in un dizionario
+fito_biosphere_exchanges = {
+    "Fungicide_harmful": 0,
+    "Herbicide_caution": 0,
+    "Herbicide_irritating": 0,
+    "Herbicide_harmful": 0,
+    "GrowthRegulator_harmful": 0,
+    "Insecticide_harmful": 0,
+    "Insecticide_toxic": 0,
+    "Molluscicide_irritating": 0
+}
+
+# Step 2: DAL DATO RICA (qui solo un esempio) estraggo le info associate a FITOGESG+
+fito_info = get_pesticide_info("Anticrittogamico", 4)
+
+chem_type = fito_info[0]['type'].capitalize()  # Prima lettera maiuscola
+tox_level = fito_info[0]['tox_level'].lower()  # Minuscolo
+key = f"{chem_type}_{tox_level}"  # Creazione della chiave (es. "Fungicide_irritating")
+
+# Step 4: if the key correspond to a biosdphere exchanges:
+if key in fito_biosphere_exchanges:
+    value = float(fito_info[0]['active_ing_per_ha'].split()[0])
+    fito_biosphere_exchanges[key] = fito_info[0]['active_ing_per_ha']
+
+
+
+# Step 2: Create the EcoWheataly production activity (Invetory phase)
 db_name = "ecowheataly"
 ecowheatalydb = bd.Database(db_name)
 
@@ -39,23 +60,24 @@ wheat_prod.new_exchange(input=('usda_item', 'work; ag. tractors for growing win 
 wheat_prod.new_exchange(input=('bentrup_item', 'application to N fertilizer use in winter wheat production systems'),
                         amount=kg_of_nitrogen, unit="kilogram", type='technosphere').save()
 
-# Add biosphere exchanges due to Fitosanitary
+# Add biosphere  exchanges due to Fitosanitary
+# ------- TO BE MODIFIED TO INSERT ONLY THOSE OBSERVED IN RICA
 fito_fluxes_code = [
-    ('a805071f-ce96-4d72-bdd5-4334d9aa3c23', Fungicide_harmful),
-    ('3850d44e-8919-47bc-9c0a-51ccc4ec9d9f', Herbicide_caution),
-    ('41a4e1fe-34ee-54d9-b049-3a60ada1ae3e', Herbicide_irritating),
-    ('e5492922-eaf5-4409-aa49-7f2a35cd0336', Herbicide_harmful),
-    ('51c7daf4-14e1-45e9-aa67-051c4ddcd9da', GrowthRegulator_harmful),
-    ('282973e4-3c2d-4a9c-a3f2-d39a5b36aa76', Insecticide_harmful),
-    ('1c0699e2-9be2-4c30-8328-fc0ad8caac58', Insecticide_toxic),
-    ('597847df-518f-4bd6-ae50-1de01f2761a4', Molluscicide_irritating)
+    ('a805071f-ce96-4d72-bdd5-4334d9aa3c23', fito_biosphere_exchanges['Fungicide_harmful']),
+    ('3850d44e-8919-47bc-9c0a-51ccc4ec9d9f', fito_biosphere_exchanges['Herbicide_caution']),
+    ('41a4e1fe-34ee-54d9-b049-3a60ada1ae3e', fito_biosphere_exchanges['Herbicide_irritating']),
+    ('e5492922-eaf5-4409-aa49-7f2a35cd0336', fito_biosphere_exchanges['Herbicide_harmful']),
+    ('51c7daf4-14e1-45e9-aa67-051c4ddcd9da', fito_biosphere_exchanges['GrowthRegulator_harmful']),
+    ('282973e4-3c2d-4a9c-a3f2-d39a5b36aa76', fito_biosphere_exchanges['Insecticide_harmful']),
+    ('1c0699e2-9be2-4c30-8328-fc0ad8caac58', fito_biosphere_exchanges['Insecticide_toxic']),
+    ('597847df-518f-4bd6-ae50-1de01f2761a4', fito_biosphere_exchanges['Molluscicide_irritating'])
 ]
 
 for code, amount in fito_fluxes_code:
     wheat_prod.new_exchange(input=('biosphere3', code), amount=amount, unit="kilogram", type='biosphere').save()
 wheat_prod.save()
 
-
+# ------------ USING OBSERVATION TO PERFORM LCA:
 # Step 3: Perform LCA
 functional_unit = {wheat_prod: 1}
 recipe = [m for m in bd.methods if 'ReCiPe 2016' in str(m) and '20180117' in str(m)]
